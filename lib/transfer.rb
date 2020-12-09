8902e95010210a8db42b9a80e9c912898a0ec245
Nabeil Lemar <nabeillemar@gmail.com>
2020-12-09 02:17:03 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -14,7 +14,7 @@ sender.valid? && receiver.valid?
 end
 
 def execute_transaction
-    if @sender.balance > @amount && @status == "pending"
+    if @sender.balance > @amount && @status == "open"
       @sender.balance -= @amount
       @receiver.balance += @amount
       @status = "complete"
