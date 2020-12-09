84a025ca4ad875f29a1f8d08e8cc40339251a822
Nabeil Lemar <nabeillemar@gmail.com>
2020-12-09 02:10:52 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -14,15 +14,15 @@ sender.valid? && receiver.valid?
 end
 
 def execute_transaction
-  if sender.balance < amount
-    @status = "rejected"
-  return "Transaction rejected. Please check your account balance."
-  elsif @status != "complete" && sender.valid?
-        @receiver.balance = @receiver.balance + amount
-        @sender.balance = @sender.balance - amount
-        @status = "complete"
-      end
+    if @sender.balance > @amount && @status == "pending"
+      @sender.balance -= @amount
+      @receiver.balance += @amount
+      @status = "complete"
+    else
+      @status = "rejected"
+      return "Transaction rejected. Please check your account balance."
     end
+  end
 
 
 def reverse_transfer
